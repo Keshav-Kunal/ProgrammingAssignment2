db48c17539ca2edd16673a35b037fa8672d514ca
Keshav-Kunal <32010762+Keshav-Kunal@users.noreply.github.com>
2017-10-22 10:23:06 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below are two functions that help compute the inverse of a matrix.
+## In case, the inverse has already been calculated and the same matrix is passed,
+## the program returns the inverse from the cache
 
-## Write a short comment describing this function
+## 'makeCacheMatrix' takes in an invertible matrix
+## and creates a special matrix that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i<-NULL
+        set<-function(y){
+                x<<-y
+                i<<-NULL
+        }
+        get<-function(){
+                x
+        }
+        setInverse<-function(invr){
+                i<<-invr
+        }
+        getInverse<-function(){
+                i
+        }
+        list(set=set,
+             get=get,
+             setInverse=setInverse,
+             getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+## 'cacheSolve' calculates the inverse of the special matrix returned by the 'makeCacheMatrix' function above.
+## If an matrix identical to the previous one is passed, then 'cacheSolve' retrieves its inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i<-x$getInverse()
+        ## Return a matrix that is the inverse of 'x'i
+        if(!is.null(i)){
+                message("getting cached matrix")
+                return(i)
+        }
+        originalMatrix<-x$get()
+        i<-solve(originalMatrix)
+        x$setInverse(i)
+        i
 }
+
+##E.g.
+##>source("cachematrix.R")
+##>abc<-makeCacheMatrix(matrix(c(3,2,0,0,0,1,2,-2,1),3,3))
+##>cacheSolve(abc)
+##     [,1] [,2] [,3]
+##[1,]  0.2  0.2    0
+##[2,] -0.2  0.3    1
+##[3,]  0.2 -0.3    0
+##
+##>cacheSolve(abc)
+##getting cached matrix
+##     [,1] [,2] [,3]
+##[1,]  0.2  0.2    0
+##[2,] -0.2  0.3    1
+##[3,]  0.2 -0.3    0
